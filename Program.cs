12f9a45792b8c6556d030ff9b12cc5920c5873eb
RISHIKESH KHARADE <152712515+rishikeshkharade@users.noreply.github.com>
2025-02-17 15:35:45 +0530

    Welcome to Unit Testing

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_Test
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Unit Testing");
+            BasicProgramForPractise basicProgram = new BasicProgramForPractise();
+            basicProgram.Add(10, 20);
+        }
+    }
+}
